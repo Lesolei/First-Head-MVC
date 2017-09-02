94b5a7c17dfb1e2027d4474994a9de30026f80be
Terry-Ren <620461@qq.com>
2017-09-02 10:12:54 +0800

    完成从控制器访问数据

---
+++ MVC_Example/Controllers/HelloWordController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Example.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        public ActionResult index()
+        {
+            return View();
+        }
+        // GET: HelloWord
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action!";
+        //}
+        /// <summary>
+        /// get welcome
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Welcome(string name,int NumTimes = 1)
+        {
+            ViewBag.Message = name;
+            ViewBag.NumTimes = NumTimes;
+            return View();
+        }
+    }
+}
