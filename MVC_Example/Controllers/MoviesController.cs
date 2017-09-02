5d672b5784a0297e2fa7f75d78748680eb743470
Terry-Ren <620461@qq.com>
2017-09-02 11:57:26 +0800

    为编辑加上安全的验证机制

--- MVC_Example/Controllers/MoviesController.cs
+++ MVC_Example/Controllers/MoviesController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using MVC_Example.Models;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using MVC_Example.Models;
 
 namespace MVC_Example.Controllers
 {
@@ -20,6 +16,7 @@ namespace MVC_Example.Controllers
         {
             return View(db.Movies.ToList());
         }
+
 
         // GET: Movies/Details/5
         //id参数一般是通过路由数据传递.
@@ -60,7 +57,7 @@ namespace MVC_Example.Controllers
             return View(movie);
         }
 
-        // GET: Movies/Edit/5
+        // GET: Movies/Edit/5       第一个为get 第二个为post
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -73,16 +70,22 @@ namespace MVC_Example.Controllers
                 return HttpNotFound();
             }
             return View(movie);
-        }
+        }//END for 第一个Edit
 
         // POST: Movies/Edit/5
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        //[ValidateAntiForgeryToken]过滤特性。该特性表示检测服务器请求是否被篡改。注意：该特性只能用于post请求，get请求无效。
+
         [HttpPost]
         [ValidateAntiForgeryToken]
+        //ValidateAntiForgeryToken用于防止伪造请求
+        //下面这个方法只被post请求调用（就是当表单被保存的时候）
         public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        //绑定（Bind）属性是另一个重要安全机制，可以防止黑客攻击(从over-posting数据到你的模型)。在bind中包含你想要更改的
         {
             if (ModelState.IsValid)
+            //isValid验证提交的表单数据是否可用于修改（编辑或更新）一个Movie对象。
             {
                 db.Entry(movie).State = EntityState.Modified;
                 db.SaveChanges();
--- MVC_Example/Models/Movie.cs
+++ MVC_Example/Models/Movie.cs
@@ -1,18 +1,35 @@
 ﻿using System;
+//实现数据验证的类DataAnnotations
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace MVC_Example.Models
+
 {
+
     public class Movie
+
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        //Display属性指明要显示的字段的名称
+        [Display(Name = "Release Date")]
+        //DataType属性用于指定类型的数据
+        [DataType(DataType.Date)]
+        //DisplayFormat属性在Chrome浏览器里有一个bug：呈现的日期格式不正确
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+
     }
+
     public class MovieDBContext : DbContext
+
     {
+
         public DbSet<Movie> Movies { get; set; }
+
     }
+
 }
